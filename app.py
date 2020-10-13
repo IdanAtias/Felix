c4898dd672716f67bb7913540defe7862346e221
Idan Atias <idan.atias@kaminario.com>
2020-10-13 23:28:10 +0300

    Rename TeamsBot to Felix
    Signed-off-by: Idan Atias <idan.atias@kaminario.com>

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ from botbuilder.core import (
 from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity, ActivityTypes
 
-from bots import TeamsBot
+from bots import Felix
 
 # Create the loop and Flask app
 from config import DefaultConfig
@@ -73,7 +73,7 @@ CONVERSATION_STATE = ConversationState(MEMORY)
 DIALOG = MainDialog(CONFIG.CONNECTION_NAME)
 
 # Create Bot
-BOT = TeamsBot(CONVERSATION_STATE, USER_STATE, DIALOG)
+BOT = Felix(CONVERSATION_STATE, USER_STATE, DIALOG)
 
 
 # Listen for incoming requests on /api/messages.
--- bots/felix.py
+++ bots/felix.py
@@ -1,6 +1,3 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
 from typing import List
 from botbuilder.core import (
     ConversationState,
@@ -14,24 +11,23 @@ from helpers.dialog_helper import DialogHelper
 from .dialog_bot import DialogBot
 
 
-class TeamsBot(DialogBot):
+class Felix(DialogBot):
     def __init__(
         self,
         conversation_state: ConversationState,
         user_state: UserState,
         dialog: Dialog,
     ):
-        super(TeamsBot, self).__init__(conversation_state, user_state, dialog)
+        super(Felix, self).__init__(conversation_state, user_state, dialog)
 
     async def on_members_added_activity(
         self, members_added: List[ChannelAccount], turn_context: TurnContext
     ):
         for member in members_added:
             # Greet anyone that was not the target (recipient) of this message.
-            # To learn more about Adaptive Cards, see https://aka.ms/msbot-adaptivecards for more details.
             if member.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity(
-                    "Welcome to AuthenticationBot. Type anything to get logged in. Type "
+                    "Welcome to Felix. Type anything to get logged in. Type "
                     "'logout' to sign-out."
                 )
 
