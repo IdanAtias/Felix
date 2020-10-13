9898cf2f5dac98457a6f9d77bcacf94872764ade
Idan Atias <idan.atias@kaminario.com>
2020-10-13 17:46:55 +0300

    Update config.py
    Use AadConnectionName env var for indicating this identity provider is
    AAD
    
    Signed-off-by: Idan Atias <idan.atias@kaminario.com>

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    CONNECTION_NAME = os.environ.get("ConnectionName", "")
+    APP_ID = os.environ["MicrosoftAppId"]
+    APP_PASSWORD = os.environ["MicrosoftAppPassword"]
+    CONNECTION_NAME = os.environ["AadConnectionName"]
