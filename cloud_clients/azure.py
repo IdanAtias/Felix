bebb3b56f2c1464a41c0d34edbcb9021977a07ee
Idan Atias <idan.atias@silk.us>
2021-07-08 11:14:26 +0300

    azure: handle cases where VM power state is not applicable
    Signed-off-by: Idan Atias <idan.atias@silk.us>

--- cloud_clients/azure.py
+++ cloud_clients/azure.py
@@ -52,12 +52,7 @@ class Vms:
 
         vms = []
         for vm_data in data["value"]:
-            vm = Vm(
-                id=vm_data["properties"]["vmId"],
-                name=vm_data["name"],
-                rg=vm_data["id"].split("/")[4],
-                power_state=vm_data["properties"]["instanceView"]["statuses"][1]["code"].split("/")[1],
-            )
+            vm = Vm.from_raw_data(data=vm_data)
             if not filter_func or filter_func(vm):
                 vms.append(vm)
 
--- cloud_models/azure.py
+++ cloud_models/azure.py
@@ -1,6 +1,9 @@
+import structlog
 from typing import Optional
 from pydantic import BaseModel
 from enum import Enum
+
+logger = structlog.get_logger(__name__)
 
 
 class AzureResource(BaseModel):
@@ -21,7 +24,21 @@ class VmPowerState(str, Enum):
     stopped = "stopped"
     deallocating = "deallocating"
     deallocated = "deallocated"
+    na = "N/A"  # not applicable for cases where power state is not available (for example, when vm provisioning fails)
 
 
 class Vm(AzureResource):
     power_state: VmPowerState
+
+    @classmethod
+    def from_raw_data(cls, data: dict):
+        vm_id = data["properties"]["vmId"]
+        name = data["name"]
+        rg = data["id"].split("/")[4]
+        try:
+            power_state = data["properties"]["instanceView"]["statuses"][1]["code"].split("/")[1]
+        except Exception:
+            logger.exception("could not extract power state for vm", vm_id=vm_id, name=name, rg=rg)
+            power_state = VmPowerState.na.value
+
+        return cls(id=vm_id, name=name, rg=rg, power_state=power_state)
